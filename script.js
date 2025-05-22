0fd39cd4e6736040792ebc00c7341d20cbe370d9
Nur Ahmad Ikmal <azmanikmal@gmail.com>
2025-05-22 15:50:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function showOptions(id) {
 
 document.addEventListener("DOMContentLoaded", function () {
   const menuItems = [
+    { name: "Spicy ButterMilk Prawn", price: 15 },
     { name: "Aglio Olio Roasted Chicken", price: 12 },
     { name: "Aglio Olio Beef Strips", price: 13 },
     { name: "Aglio Olio Prawn", price: 15 },
